7878a7ce8a6d4dd663e11e676860727a039278f3
Swostik Pati <swostikpati@gmail.com>
2023-02-16 03:56:21 +0400

    added basic comments in code

--- Assignment1_30MFF/app.js
+++ Assignment1_30MFF/app.js
@@ -1,6 +1,8 @@
+// Making an IntersectionObserver object and defining a specific funtion to excute
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
+        // Switching between adding and removing the show-steps class based
         if (entry.isIntersecting) {
             entry.target.classList.add("show-steps");
         }
@@ -11,7 +13,9 @@ const observer = new IntersectionObserver((entries) => {
 });
 // reference: https://www.youtube.com/watch?v=T33NN_pPeNI
 
+// Selecting DOM Elements
 const hiddenSteps = document.querySelectorAll(" .hidden-steps");
 
+// Executing the observe function for each element with the specific class
 hiddenSteps.forEach((elem) => observer.observe(elem));
 
