941d955254fe7c104f2f5d10986d4a8c8b582943
Swostik Pati <swostikpati@gmail.com>
2023-02-17 21:08:17 +0400

    added display property of navigation arrow

--- Assignment1_30MFF/app.js
+++ Assignment1_30MFF/app.js
@@ -1,7 +1,7 @@
 // Making an IntersectionObserver object and defining a specific funtion to excute
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
+        // console.log(entry);
         // Switching between adding and removing the show-steps class based
         if (entry.isIntersecting) {
             entry.target.classList.add("show-steps");
@@ -15,7 +15,20 @@ const observer = new IntersectionObserver((entries) => {
 
 // Selecting DOM Elements
 const hiddenSteps = document.querySelectorAll(" .hidden-steps");
+const up_arrow = document.querySelector("#top_arrow");
 
 // Executing the observe function for each element with the specific class
 hiddenSteps.forEach((elem) => observer.observe(elem));
 
+// To find the location of the scrollbar
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+
+    // When the scrollbar hits a particular point in the website, toggle between display block and none
+    if (scroll > 900) {
+        up_arrow.style.display = "block";
+    }
+    else {
+        up_arrow.style.display = "none";
+    }
+});
